1546b733c61ea4fd8b83db7c529f7e7407499162
Reza <mohammad.r.moradi@gmail.com>
2022-08-15 19:49:00 +0200

    feat: get players name from user

--- src/features/slicers/board-slice.tsx
+++ src/features/slicers/board-slice.tsx
@@ -1,6 +1,7 @@
 import { PayloadAction,createSlice} from '@reduxjs/toolkit'
 
 interface BoardState {
+    playerNames: (string | null)[]
     xIsNext: boolean,
     winner: string | null,
     board: (string | null)[]
@@ -8,6 +9,7 @@ interface BoardState {
 }
 
 const initialState: BoardState = {
+    playerNames: Array(2).fill(null),
     xIsNext: true,
     winner: null,
     board: Array(9).fill(null)
@@ -17,6 +19,9 @@ const boardSlice = createSlice({
     name: 'board',
     initialState,
     reducers: {
+        'setPlayersName': (state: BoardState, action: PayloadAction<string[]>) => {
+            state.playerNames = action.payload
+        },
         'nextMove': (state: BoardState, action:PayloadAction<number>) => {
             if (state.xIsNext) {
                 state.board[action.payload] = 'X'
@@ -31,12 +36,12 @@ const boardSlice = createSlice({
             for (let i = 0; i < condition.length; i++){
                 const [a, b, c] = condition[i]
                 if (state.board[a] != null && state.board[a] == state.board[b] && state.board[a] == state.board[c]) {
-                    state.winner = state.board[a]
+                    state.winner = state.board[a]=='X'?state.playerNames[0]:state.playerNames[1]
                 }
             }
         }
     }
 })
 
-export const { nextMove, calWinner } = boardSlice.actions
+export const { setPlayersName, nextMove, calWinner } = boardSlice.actions
 export default boardSlice.reducer
